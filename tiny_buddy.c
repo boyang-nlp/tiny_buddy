f1a1900d93b28f5935d404cc7d4e60eeb5d0091d
os_lab_user <you@example.com>
2016-02-15 23:00:38 +0800

    modified:   tiny_buddy.c 	modified:   tiny_buddy.h

--- tiny_buddy.c
+++ tiny_buddy.c
@@ -1,116 +1,242 @@
-/*
- * tiny_buddy.c
- *
- *  Created on: 2016年1月29日
- *      Author: boyang
+/*Tiny Buddy System
+ *Author : dingboyang
+ *Date   : 2016.1.29
  */
 #include <pmm.h>
-#include <string.h>
 #include "tiny_buddy.h"
-Buddy *mem;
-uint8_t MAX_ORD;
-unsigned int nr_free;
+Buddy *mem;					/*Buddy CBT*/
+uint8_t MAX_ORD;			/*The maximum  order of memory block*/
+bool mem_init;				/*The CBT initialized flag*/
+unsigned int nr_free;		/*Free memory block*/
+/* update - Update ancestors' allocable status when alloc or free a
+ * block
+ * i : Buddy index in mem;
+ */
 
-static void update(Buddy* mem, int i){
+static void update(int i){
 	while(i){
-		if(i % 2){
-			if(free(mem[i]) && free(mem[sib(i)]))
-				set_left( mem[par(i)], ord( mem[par(i)]) - 1 );
-			else
-				set_left( mem[par(i)], max( allocable(mem[i]), allocable(mem[sib(i)])) );
-		}
-		else{
-			if(free(mem[i]) && free(mem[sib(i)]))
-				set_right( mem[par(i)], ord(mem[par(i)]) - 1 );
-			else
-				set_right( mem[par(i)], max( allocable(mem[i]), allocable(mem[sib(i)]) ) );
-		}
 		i = par(i);
+		set_lft(mem[i], allocable(mem[lc(i)]));
+		set_rht(mem[i], allocable(mem[rc(i)]));
 	}
 }
-static uint8_t get_ord(size_t n){//MIN_BLOCK << (_ord - 1) >= n
-	uint8_t _ord = 0;
-	while( (MIN_BLOCK<<_ord) < n)_ord++;
-	return _ord;
+/*get_ord - Get the order of a memory block with n bytes. 
+ */
+static uint8_t get_ord(size_t n){
+	uint8_t o = 0;
+	while( (MIN_BLOCK << o) < n)o++;
+	return o + 2;
 }
+/*idx2ord - Get the order of mem[i].
+ */
+static uint8_t idx2ord(size_t i){
+	uint8_t cnt = 0;
+	while(i){
+		i = par(i);
+		cnt++;
+	}
+	return MAX_ORD - cnt;
+}
+/*buddy_fill - Flag all buddy as unallocatable which belong subtree i.
+ *             Used to initialize memory.
+ */
 static void buddy_fill(uint32_t i){
-	uint8_t _ord = ord(mem[i]);
+	uint8_t o = idx2ord(i);
 	uint32_t k,j;
-	for(k = 0;k<_ord;k++)
+	for(k = 0;k<o;k++,i<<=1)
 		for(j = 0;j < (1 << k); j++)
-			set_buddy(mem[i + j + (1 << k) - 1],_ord, 0, 0);
+			set_buddy(mem[i + j + (1 << k) - 1],o - k, 0, 0);
 }
-static void buddy_clean(uint32_t i){
-	uint8_t _ord = ord(mem[i]);
-	uint32_t k,j;
-	for(k = 0;k<_ord;k++)
-		for(j = 0;j < (1<<k); j++)
-			set_buddy(mem[i + j + (1<<k) - 1],_ord, _ord - 1, _ord - 1);
-}
+/*buddy_init - Buddy memory manager initialization*/
 void buddy_init(){
 	MAX_ORD = get_ord(KMEMSIZE);
+	mem_init = 0;
 	nr_free = 0;
-	buddy_fill(0);//unusable
 }
+/*buddy_init_memmap - Set the detected physical memory block allocable*/
 static void buddy_init_memmap(struct Page* page, size_t n){
     assert(n > 0);
+    if(!mem_init){
+		mem = page2kva(page);
+		uint32_t mem_sz = (shl(1, MAX_ORD-1)-1) * sizeof(Buddy);
+		uint32_t pgnum = ROUNDUP(mem_sz, PGSIZE) / PGSIZE;
+		page += pgnum;
+		n -= pgnum;
+		mem_init = 1;
+		buddy_fill(0);
+    }
     struct Page *p = page;
-    for (; p != page + n; p ++) {
+    nr_free += n;
+    uint32_t idx;
+    for (; p != page + n; p++) {
         assert(PageReserved(p));
         p->flags = p->property = 0;
+		idx = page2buddy(p, 2);
+		set_buddy(mem[idx], 2, 1, 1);
+		update(idx);
         set_page_ref(p, 0);
     }
-	uint8_t _ord = get_ord(n << MIN_AOFF);
-	do{
-		if( ( 1 << ( _ord - 1 ) ) & n ){
-			buddy_clean(page2buddy(page, _ord));
-			page += ( 1 << (_ord - 1) ) & n;
-		}
-	}while(--_ord > 0);
 }
+/*buddy_alloc_pages - alloc n page from free area and return the first page.
+ *	                  The header page should set page property flag, and the
+ *					  property field should assign to n which means next 
+ *					  follow n pages has been allocated.
+ */
 struct Page* buddy_alloc_pages(size_t n){
+	assert(n > 0);
+	if(n > nr_free){
+		return NULL;
+	}
 	struct Page* page = NULL;
-	uint8_t _ord = get_ord(n << MIN_AOFF);
+	uint8_t o = get_ord(n * MIN_BLOCK);
 	uint32_t i = 0;
 	while(1){
-		if( left(mem[i]) >= _ord )
+		if( lft(mem[i]) >= o )
 			i = lc(i);
-		else if( right(mem[i]) >= _ord )
+		else if( rht(mem[i]) >= o )
 			i = rc(i);
 		else
 			break;
-	}
-	if( _ord == order(mem[i]) ){
+	}	
+	if( o == ord(mem[i]) ){
 		set_rht(mem[i], 0);
 		set_lft(mem[i], 0);
 		page = buddy2page(mem[i], i);
+		assert(!PageReserved(page));
 		SetPageProperty(page);
-		nr_free -= page->property = 1 << (order(mem[i]) - 1);
-		update(mem, i);
+		nr_free -= page->property = 1 << (ord(mem[i]) - 2);
+		update(i);
 	}
 	return page;
 }
+/*buddy_free_pages - Free continous pages start with page.The header page's
+					 property flag should be unset, and the property field 
+					 should assign to zero.
+ */
 void buddy_free_pages(struct Page* page){
-	uint32_t i = page2buddy(page, get_order(page->property * MIN_BLOCK));
-	set_rht(mem[i], order(mem[i])-1);
-	set_lft(mem[i], order(mem[i])-1);
-	update(mem, i);
+	assert(!PageReserved(page) && PageProperty(page));
+	uint32_t i = page2buddy(page, get_ord(page->property * MIN_BLOCK));
+	set_rht(mem[i], ord(mem[i])-1);
+	set_lft(mem[i], ord(mem[i])-1);
+	update(i);
 	nr_free += page->property;
 	ClearPageProperty(page);
 	page->property = 0;
 }
-
+void __buddy_free_pages(struct Page* page, uint32_t n){
+	buddy_free_pages(page);
+}
 static size_t
 buddy_nr_free_pages(void) {
     return nr_free;
 }
 
-static void basic_check(void){
-	//
-}
-
+/*buddy_check - Validate the correctness of buddy system.
+ */
 static void buddy_check(void){
-	//
+	assert((1 << (MAX_ORD-2)) >= KMEMSIZE/MIN_BLOCK && (1 << (MAX_ORD-3)) < KMEMSIZE/MIN_BLOCK);
+	uint32_t backup = nr_free;
+	struct Page* p1 = buddy_alloc_pages(1);
+	assert(backup == nr_free + 1);
+	assert(PageProperty(p1));
+	assert(p1->property == 1);
+	backup = nr_free;
+	struct Page* p2 = buddy_alloc_pages(3);
+	assert(backup == nr_free + 4);
+	assert(PageProperty(p2));
+	assert(p2->property == 4);
+	backup = nr_free;
+	buddy_free_pages(p1);
+	assert(backup == nr_free - 1);
+	assert(!PageProperty(p1));
+	assert(p1->property == 0);
+	backup = nr_free;
+	buddy_free_pages(p2);
+	assert(backup == nr_free - 4);
+	backup  = nr_free;
+	p1 = buddy_alloc_pages(53);
+	assert(backup == nr_free + 64);
+	assert(PageProperty(p1));
+	assert(p1->property == 64);
+	backup = nr_free;
+	p2 = buddy_alloc_pages(896);
+	assert(backup == nr_free + 1024);
+	assert(PageProperty(p2));
+	assert(p2->property == 1024);
+	backup = nr_free;
+	buddy_free_pages(p1);
+	assert(backup == nr_free - 64);
+	assert(!PageProperty(p1));
+	assert(p1->property == 0);
+	backup = nr_free;
+	buddy_free_pages(p2);
+	assert(backup == nr_free - 1024);
+	assert(!PageProperty(p2));
+	assert(p2->property == 0);
+	
+	
+	backup = nr_free;
+	p1 = buddy_alloc_pages((1<<13) - 13);
+	assert(backup == nr_free + (1<<13));
+	assert(PageProperty(p1));
+	assert(p1->property == (1<<13));
+	
+	backup = nr_free;
+	p2 = buddy_alloc_pages((1<<13) - 13);
+	assert(backup == nr_free + (1<<13));
+	assert(PageProperty(p2));
+	assert(p1->property == (1<<13));
+
+	struct Page *p3, *p4;
+	backup = nr_free;
+	p3 = buddy_alloc_pages((1<<12) - 12);
+	assert(backup == nr_free + (1<<12));
+	assert(PageProperty(p3));
+	assert(p3->property == (1<<12));
+
+	backup = nr_free;
+	p4 = buddy_alloc_pages((1<<12) - 12);
+	assert(backup == nr_free + (1<<12));
+	assert(PageProperty(p4));
+	assert(p4->property == (1<<12));
+	
+	backup = nr_free;
+	buddy_free_pages(p1);
+	assert(backup == nr_free - (1<<13));
+	assert(!PageProperty(p1));
+	assert(p1->property == 0);	
+	
+	backup = nr_free;	
+	buddy_free_pages(p2);
+	assert(backup == nr_free - (1<<13));
+	assert(!PageProperty(p2));
+	assert(p2->property == 0);	
+	
+	backup = nr_free;	
+	buddy_free_pages(p3);
+	assert(backup == nr_free - (1<<12));
+	assert(!PageProperty(p3));
+	assert(p3->property == 0);	
+
+	backup = nr_free;	
+	buddy_free_pages(p4);
+	assert(backup == nr_free - (1<<12));
+	assert(!PageProperty(p4));
+	assert(p4->property == 0);
+	
+	struct Page* p[100];
+	int i;
+	for(i = 0;i<100;i++){
+		p[i] = buddy_alloc_pages(1);
+		assert(!PageReserved(p[i]));
+		assert(PageProperty(p[i]));
+		assert(p[i]->property == 1);
+	}
+	for(i = 0;i<100;i++){
+		buddy_free_pages(p[i]);
+		assert(!PageProperty(p[i]));
+		assert(p[i]->property == 0);
+	}
 }
 
 const struct pmm_manager tiny_buddy_pmm_manager = {
@@ -118,7 +244,7 @@ const struct pmm_manager tiny_buddy_pmm_manager = {
 	.init = buddy_init,
 	.init_memmap = buddy_init_memmap,
 	.alloc_pages = buddy_alloc_pages,
-	.free_pages = buddy_free_pages,
+	.free_pages = __buddy_free_pages,
 	.nr_free_pages = buddy_nr_free_pages,
 	.check = buddy_check,
 };
--- tiny_buddy.h
+++ tiny_buddy.h
@@ -1,16 +1,14 @@
-/*
- * tiny_buddy.h
- *
- *  Created on: 2016年1月29日
- *      Author: lenovo8
+/*Tiny Buddy System.
+ *Author : dingboyang
+ *Date   : 2016.1.29
  */
-
 #ifndef LAB2_KERN_MM_TINY_BUDDY_H_
 #define LAB2_KERN_MM_TINY_BUDDY_H_
 typedef uint16_t Buddy;
+
 /****************************************************************************
- *Buddy Data Type as an abstract of memory.
- *We organize whole memory space as complete binary tree, every node of it is
+ *Buddy Data Type is an abstract of memory.
+ *We organize whole memory space as complete binary tree(CBT), every node of it is
  *a buddy. Buddy is a memory chunk with the size of power of 2.
  *The data type Buddy is actually an unsigned short interger with three field
  *represented as follow:
@@ -24,47 +22,53 @@ typedef uint16_t Buddy;
  ****************************************************************************/
 
 /*Minium memory block is 4KB(1 PAGE)*/
-#define        MIN_AOFF       12
-#define        MIN_BLOCK      shl(1, MIN_AOFF)
+#define        MIN_AOFF        12//
+#define        MIN_BLOCK       shl(1, MIN_AOFF)//
 
-#define        shr(e, i)      ( (e) >> (i) )
-#define        shl(e, i)      ( (e) << (i) )
-#define         or(a, b)      ( (a) |  (b) )
-#define        and(a, b)      ( (a) &  (b) )
-#define        not(e)         (~(e))
-
+#define        shr(e, i)       ( (e) >> (i) )
+#define        shl(e, i)       ( (e) << (i) )
+#define         or(a, b)       ( (a) |  (b) )
+#define        and(a, b)       ( (a) &  (b) )
+#define        not(e)          (~(e))
+#define        _max(a, b)       ( (a) > (b) ? (a) : (b))
+#define	       set(e, mask, v) ( (e) = or( and(e, not(mask)), v ) )
 /*Get the order,left,right field of buddy*/
 #define        ord(buddy)     and( shr(buddy, 10), 0x001f )
 #define        lft(buddy)     and( shr(buddy,  5), 0x001f )
 #define        rht(buddy)     and( buddy, 0x001f )
 
 /*Set right,left or order field of buddy*/
-#define    set_rht(buddy, i)  ( buddy = or( and(buddy, not(0x03e0) ), shl( and(i, 0x1f), 5) ) )
+#define	   set_ord(buddy, o)  ( set( buddy, shl( 0x1f, 10 ), shl( o, 10 ) ) )
+#define    set_lft(buddy, l)  ( set( buddy, shl( 0x1f,  5 ), shl( l,  5 ) ) ) 
 
-#define    set_lft(buddy, i)  ( buddy = or( and(buddy, not(0x001f) ), and( i, 0x1f) ) )
+#define    set_rht(buddy, r)  ( set( buddy, 0x1f, r ) )
 
-#define  set_buddy(buddy, ord, lft, rht)\
-							  ( set_rht(buddy, rht), set_lft(buddy, lft), \
-							    buddy = or( and(buddy, not(0x7c00) ), shl( and(ord, 0x1f), 10) ) )
+#define  set_buddy(buddy, o, l, r)\
+							  ( set_ord(buddy, o), set_lft(buddy, l), set_rht(buddy, r))
 
 /*Get a buddy's right,left child index*/
-#define         rc(i)         shr( i + 1, 1)
+#define         rc(i)         shl( i + 1, 1)
 #define         lc(i)         ( rc(i) - 1 )
 /*Get a parent,sibling index of a buddy*/
 #define        par(i)         shr(i-1, 1)
-#define        sib(i)         ( ( i % 2 ) ? ( i-1 ) : ( i+1 ) )
+#define        sib(i)         ( ( i % 2 ) ? ( i+1 ) : ( i-1 ) )
 /*Judge if a buddy is totally free*/
-#define 	 free(buddy)	  ( lft(buddy) == rht(buddy) == (ord(buddy)-1) )
-/*Judge if a buddy could still alloc a chunk*/
-#define  allocable(buddy)     ( free(buddy) ? order(buddy) : max( left(buddy), right(buddy) ) )
+#define 	 free(buddy)	  ( ( lft(buddy) == rht(buddy) ) && ( rht(buddy) == (ord(buddy)-1) ) )
+/*Judge if a buddy could still alloc a memory chunk*/
+#define  allocable(buddy)     ( free(buddy) ? ord(buddy) : _max( lft(buddy), rht(buddy) ) )
 
-/*Get the address prefix of a buddy*/
-#define     prefix(buddy)     ( MAX_ORD - order(buddy) )
-/*Convert buddy to physical address or page*/
-#define   buddy2pa(buddy, i)  ( ( ( ( 1 << prefix(buddy) ) - 1) & (i + 1) ) << (32 - prefix(buddy) - 1) )
+/*Get the buddy's start physical address*/
+#define buddy2pa(buddy, i)    ( shl(and( i + 1 , shl(1, MAX_ORD - ord(buddy)) - 1 ) , ord( buddy ) - 2 +MIN_AOFF) )
+/*Get the buddy's start physical address, and convert it to
+ *page struct
+ */
 #define buddy2page(buddy, i)  pa2page( buddy2pa( buddy, i ) )
-/*Convert physical address or page to buddy index in mem*/
-#define   pa2buddy(pa, ord)   ( ( shr( pa, ord +  MIN_AOFF - 2) + 1 << ( MAX_ORD - ord ) ) - 1 )//
-#define page2buddy(page, ord) pa2buddy( page2pa( page ), ord )
+/*Get the corresponding buddy index in mem of a memory with the 
+  start physical pa and the order o.*/
+#define pa2buddy(pa, o)       ( shr( pa,  (o) - 2 + MIN_AOFF ) + shl( 1, MAX_ORD - (o) ) - 1 )
+/*Get the corresponding buddy index in mem of a memory block  
+  with order o and start with page.*/
+#define page2buddy(page, o)   ( pa2buddy( page2pa( page ), o ) )
 
+extern const struct pmm_manager tiny_buddy_pmm_manager;
 #endif /* LAB2_KERN_MM_TINY_BUDDY_H_ */
